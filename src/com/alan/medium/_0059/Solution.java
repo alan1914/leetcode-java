c6219ed98124deb7786eb7a4598e931edcd65836
db0n15 <db0n15@163.com>
2022-02-24 15:25:38 +0800

    feat: add 59

---
+++ src/com/alan/medium/_0059/Solution.java
@@ -0,0 +1,25 @@
+package com.alan.medium._0059;
+
+/**
+ * @author shiml
+ * @date 2022-2-24 14:10:38
+ */
+public class Solution {
+
+    public int[][] generateMatrix(int n) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        Solution solution = new Solution();
+        int[][] result = solution.generateMatrix(n);
+        System.out.println(result);
+
+    }
+
+}
